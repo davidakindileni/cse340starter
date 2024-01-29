5897f5de084e299b3373cca6c0fd04324b7e31bc
David Akindileni <davidakindileni@yahoo.co.uk>
2024-01-29 11:19:53 +0100

    complete assignment 1

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+//jshint esversion:6
+
 /* ******************************************
  * This server.js file is the primary file of the 
  * application. It is used to control the project.
@@ -5,16 +7,28 @@
 /* ***********************
  * Require Statements
  *************************/
+const expressLayouts = require("express-ejs-layouts")
 const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
 /* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+
+/* ***********************
  * Routes
  *************************/
 app.use(static)
 
+// Index route
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
+})
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
